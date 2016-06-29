38ce96b28203708557c9eb3259459e90a08b79d5
Hien Dang <hienpd@outlook.com>
2016-06-29 14:01:54 -0700

    Save point

--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,89 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+// GET /authors
+router.get('/authors', (_req, res, next) => {
+  knex('authors')
+    .orderBy('id')
+    .then((authors) => {
+      res.send(authors);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// GET /authors/3
+router.get('/authors/:id', (req, res, next) => {
+  knex('authors')
+  .where('id', req.params.id)
+  .first()
+  .then((author) => {
+    res.send(author);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+// POST /authors
+router.post('/authors', (req, res, next) => {
+  knex('authors')
+    .insert(req.body, '*')
+    .then((authors) => {
+      res.send(authors[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// PATCH /authors/3
+router.patch('/authors/:id', (req, res, next) => {
+  knex('authors')
+    .update(req.body, '*')
+    .where('id', req.params.id)
+    .then((authors) => {
+      res.send(authors[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// DELETE /authors/3
+router.delete('/authors/:id', (req, res, next) => {
+  knex('authors')
+    .where('id', req.params.id)
+    .first()
+    .then((author) => {
+
+      return knex('authors')
+      .del()
+      .where('id', req.params.id)
+      .then(() => {
+        delete author.id;
+        res.send(author);
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// GET /authors/3/books
+router.get('/authors/:id/books', (req, res, next) => {
+  knex('books')
+    .where('author_id', req.params.id)
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,76 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+// GET /books
+router.get('/books', (_req, res, next) => {
+  knex('books')
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// GET /books/3
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+  .where('id', req.params.id)
+  .first()
+  .then((book) => {
+    res.send(book);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+// POST /books
+router.post('/books', (req, res, next) => {
+  knex('books')
+    .insert(req.body, '*')
+    .then((books) => {
+      res.send(books[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// PATCH /books/3
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .update(req.body, '*')
+    .where('id', req.params.id)
+    .then((books) => {
+      res.send(books[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// DELETE /books/3
+router.delete('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+
+      return knex('books')
+      .del()
+      .where('id', req.params.id)
+      .then(() => {
+        delete book.id;
+        res.send(book);
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
