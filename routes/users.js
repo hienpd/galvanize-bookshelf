78701627ca805a4f723f2d3ab29b6f0c7a957e25
Hien Dang <hienpd@outlook.com>
2016-06-29 14:56:20 -0700

    Add user registration

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,26 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const knex = require('../knex');
 
 router.post('/users', (req, res, next) => {
   bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
     if (hashErr) {
       return next(hashErr);
     }
-
-    console.log(req.body.email, hashed_password);
-    res.sendStatus(200);
+    knex('users')
+      .insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        hashed_password: hashed_password
+      })
+      .then((users) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        next(err);
+      });
   });
 });
 
