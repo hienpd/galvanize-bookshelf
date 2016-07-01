61d2945f40f59333b3e4c019e95a4824b75a1d79
Hien Dang <hienpd@outlook.com>
2016-07-01 15:56:34 -0700

    Save point before git pull upstream master

--- routes/session.js
+++ routes/session.js
@@ -2,7 +2,46 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
-// YOUR CODE HERE
+router.post('/session', (req, res, next) => {
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then((user) => {
+      // Check whether user email exists
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      // Check if password is right
+      const hashed_password = user.hashed_password;
+
+      bcrypt.compare(req.body.password, hashed_password, (err, isMatch) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (!isMatch) {
+          return res.sendStatus(401);
+        }
+
+        // If email and password is ok
+        req.session.userId = user.id;
+        res.cookie('loggedIn', true);
+        res.sendStatus(200);
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/session', (req, res, next) => {
+  req.session = null;
+  res.clearCookie('loggedIn');
+  res.sendStatus(200);
+});
 
 module.exports = router;
--- routes/users_books.js
+++ routes/users_books.js
@@ -2,7 +2,61 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+const checkAuth = function(req, res, next) {
+  if (!req.session.userId) {
+    return res.sendStatus(401);
+  }
+
+  next();
+}
+
+router.get('/users/books', checkAuth, (req, res, next) => {
+  knex('books')
+    .innerJoin('users_books', 'users.books.book_id', 'books.id')
+    .where('users_books.user_id', req.session.userId)
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  knex('books')
+    .innerJoin('users_books', 'users_books.book_id', 'books.id')
+    .where({
+      'books.id': bookId,
+      'users_books.user_id': userId;
+      // Fill in
+    });
+});
+
+router.post('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const bookId = Number.parseInt(req.params.bookId);
+  const userId = req.session.user.id;
+
+  knex('users_artists')
+    .insert({
+      user_id: userId,
+      book_id: bookId
+    }, '*')
+    .then((results) => {
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const bookId = Number.parseInt(req.params.bookId)
+  // Fill in
+});
 
 module.exports = router;
